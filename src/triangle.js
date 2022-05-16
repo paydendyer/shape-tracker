331741b5ac252dc1930c3cdda19f9c7c31f4f9b9
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-05-15 19:23:03 -0500

    Passed 3rd test

--- src/triangle.js
+++ src/triangle.js
@@ -3,6 +3,7 @@ export default function Triangle(side1, side2, side3) {
   this.side2 = side2;
   this.side3 = side3;
 }
+//conditional that checks whether the value of one side is greater than the sum of the other two sides
 Triangle.prototype.checkType = function() {
   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
     return "not a triangle";
